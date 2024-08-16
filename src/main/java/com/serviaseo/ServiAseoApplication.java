181a2df028414678f6108dc9dc69ce1dfdfd2335
Camilo Pinzon <capinzonr@unal.edu.co>
2024-08-16 04:42:51 -0500

    First commit

---
+++ src/main/java/com/serviaseo/ServiAseoApplication.java
@@ -0,0 +1,13 @@
+package com.serviaseo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiAseoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiAseoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serviaseo/ServiAseoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serviaseo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiAseoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
